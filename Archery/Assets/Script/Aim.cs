fa19df55578d15b528f1a496588acb8455d3ed03
marshmann <marshmann@duq.edu>
2018-10-04 12:03:21 -0400

    Initial Commit

---
+++ Archery/Assets/Script/Aim.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Aim : MonoBehaviour {
+    private Rigidbody rb;
+    [SerializeField] float verSensitivity = 2;
+    [SerializeField] float horSensitivity = 2;
+    [SerializeField] float speed = 0.5f;
+    [SerializeField] Camera cam;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+        Cursor.visible = false;
+    }
+
+    private void Update() {
+        CheckForCameraMovement();
+
+        //If you're not aiming 
+        if (!Input.GetMouseButton(0)) {
+            CheckForMovement(speed);
+        }
+        else CheckForMovement(speed / 2);
+    }
+    private void CheckForMovement(float moveSpeed) {        
+        rb.MovePosition(transform.position + (transform.right * Input.GetAxis("Vertical") * moveSpeed) 
+            + (transform.forward * -Input.GetAxis("Horizontal") * moveSpeed));        
+    }
+
+    private void CheckForCameraMovement() {
+        float mouseX = Input.GetAxisRaw("Mouse X");
+        float mouseY = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 rotateX = new Vector3(mouseY * verSensitivity, 0, 0);
+        Vector3 rotateY = new Vector3(0, mouseX * horSensitivity, 0);
+
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotateY));
+        cam.transform.Rotate(-rotateX);
+    }
+}
---
+++ Archery/Assets/Script/Embed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Embed : MonoBehaviour {
+    [SerializeField] GameObject sparks;
+    private Rigidbody rb;
+    private bool isEmbeded = false;
+    private GameObject x;
+
+    private void Start() { rb = GetComponent<Rigidbody>(); }
+
+    private void Update() {
+        if (isEmbeded && !x.GetComponent<ParticleSystem>().isEmitting) {
+            Destroy(x, 1f); isEmbeded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision col) {
+        transform.GetComponent<ProjectileForce>().enabled = false;
+        rb.velocity = Vector3.zero;
+        rb.isKinematic = true;
+
+        x = Instantiate(sparks, transform) as GameObject;
+        sparks.transform.rotation = transform.rotation;
+        isEmbeded = true;
+    }
+}
